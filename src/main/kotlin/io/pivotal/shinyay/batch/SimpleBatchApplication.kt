6665545cffd30ebe22ba49b3aee52540c081f23b
shinyay <shinya.com@gmail.com>
2019-05-07 22:37:36 +0900

    Add Batch Processing Enabled

--- src/main/kotlin/io/pivotal/shinyay/batch/SimpleBatchApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/batch/SimpleBatchApplication.kt
@@ -1,9 +1,11 @@
 package io.pivotal.shinyay.batch
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBatchProcessing
 class SimpleBatchApplication
 
 fun main(args: Array<String>) {
