f7d606e4f9a8c613db36d0c4e579a2cb3c4c850e
shinyay <shinya.com@gmail.com>
2019-05-08 07:17:36 +0900

    Add enmployee entity

---
+++ src/main/kotlin/io/pivotal/shinyay/batch/entity/Employee.kt
@@ -0,0 +1,8 @@
+package io.pivotal.shinyay.batch.entity
+
+data class Employee (
+        val id: String,
+        val employeeNumber: String,
+        val salary: String,
+        val department: String
+)
